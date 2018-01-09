a827104764d29493c9c74d3f5bb72149d39d4531
cdswyda <cdswyda@hotmail.com>
2018-01-09 16:37:38 +0800

    init

---
+++ cloud.js
@@ -0,0 +1,100 @@
+let AV = require('leanengine');
+let api = require('./getData.js');
+
+const http = require('http');
+const querystring = require('querystring');
+
+// const postData = querystring.stringify({
+//     user: JSON.stringify(
+//         [{
+//             name: '测试用户名',
+//             email: '516321242@qq.com'
+//         }, {
+//             name: '测试',
+//             email: '345566251@qq.com'
+//         }]
+//     ),
+//     type: 'all'
+// });
+/**
+ * 一个简单的云代码方法
+ */
+AV.Cloud.define('hello', function(request) {
+  return 'Hello world!';
+});
+
+// 发起一个post请求
+function post(data) {
+  const options = {
+    hostname: 'fe.epoint.com.cn',
+    port: 8080,
+    path: '/fedemo/pages/weeklyreport/mail/sendmail.php',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+      'Content-Length': Buffer.byteLength(data)
+    }
+  };
+
+  const req = http.request(options, res => {
+    console.log(`STATUS: ${res.statusCode}`);
+    console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
+    res.setEncoding('utf8');
+    res.on('data', chunk => {
+      console.log(`BODY: ${chunk}`);
+    });
+    res.on('end', () => {
+      console.log('No more data in response.');
+    });
+  });
+
+  req.on('error', e => {
+    console.error(`problem with request: ${e.message}`);
+  });
+
+  // write data to request body
+  req.write(data);
+  req.end();
+}
+
+// 给所有需要填写日志的人发邮件
+AV.Cloud.define('sendEmail', function(request) {
+  qpi.getAllUsers().then(result => {
+    let users = [];
+    result.forEach(item => {
+      // 排除不用填写日志的人
+      if (!item.attributes.noReport && item.attributes.email) {
+        users.push({
+          name: item.attributes.username,
+          email: item.attributes.email
+        });
+      }
+    });
+    console.log('=======================0');
+    console.log('准备给所有用户发送邮件：');
+    console.log(JSON.stringify(users, 0, 4));
+
+    // 组织数据发请求
+    const data = querystring.stringify({
+      user: JSON.stringify(users),
+      type: 'all'
+    });
+    post(data);
+  });
+});
+
+AV.Cloud.define('sendEmailAgain', function(request) {
+  api.getUnSubmitUsers().then(users => {
+    console.log('=======================0');
+    console.log('准备给未提交用户发送邮件：');
+    console.log('未提交用户为:');
+    console.log(JSON.stringify(users, 0, 4));
+
+    post(
+      querystring.stringify({
+        type: 'unsubmit',
+        user: JSON.stringify(users)
+      })
+    );
+  });
+});
---
+++ getData.js
@@ -0,0 +1,65 @@
+let AV = require('leanengine');
+let Promise = require('bluebird');
+
+module.exports = {
+  getAllUsers() {
+    return new AV.Query('_User').find();
+  },
+  getCurrWeekReport() {
+    const day = new Date().getDay();
+    const date = new Date().getDate();
+
+    let startDate = new Date();
+    let endDate = new Date();
+
+    startDate.setDate(date - day + 1);
+    startDate.setHours(0);
+    startDate.setMinutes(0);
+    startDate.setSeconds(0);
+    startDate.setMilliseconds(0);
+
+    endDate.setDate(date + (7 - day));
+    endDate.setHours(23);
+    endDate.setMinutes(59);
+    endDate.setSeconds(59);
+    endDate.setMilliseconds(999);
+
+    console.log('本周开始时间：' + startDate);
+    console.log('本周结束时间：' + endDate);
+
+    let q = new AV.Query('Logs');
+
+    q.greaterThanOrEqualTo('updatedAt', startDate);
+    q.lessThanOrEqualTo('updatedAt', endDate);
+
+    return q.find();
+  },
+  getUnSubmitUsers() {
+    return Promise.all([this.getAllUsers(), this.getCurrWeekReport()]).then(
+      results => {
+        let users = results[0];
+        let reports = results[1];
+        let submitUser = {};
+        let unsubmitUsers = [];
+        reports.forEach(item => {
+          submitUser[item.attributes.userId] = true;
+        });
+
+        users.forEach(user => {
+          if (
+            !submitUser[user.id] &&
+            user.attributes.noReport &&
+            user.attributes.eamil
+          ) {
+            unsubmitUsers.push({
+              name: user.username,
+              email: user.attributes.eamil
+            });
+          }
+        });
+
+        return unsubmitUsers;
+      }
+    );
+  }
+};
